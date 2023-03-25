a15f4863651a369ac12726cf6bb7cc0b4622b232
mkadr5 <muhammetkadir52@gmail.com>
2023-03-25 17:39:22 +0300

    Initial Commit

---
+++ SwiftDependencyInject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDependencyInject
+//
+//  Created by Muhammet Kadir on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
